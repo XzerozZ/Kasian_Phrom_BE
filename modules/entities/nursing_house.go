9f258e26f6536362bf83cfc42ea458f4a7f4e68c
XzerozZ <111548296+XzerozZ@users.noreply.github.com>
2025-01-05 16:19:59 +0700

    edit entity of Nursing house : no type

--- modules/entities/nursing_house.go
+++ modules/entities/nursing_house.go
@@ -4,11 +4,11 @@ import "time"
 
 type NursingHouse struct {
 	ID				string		`json:"nh_id" gorm:"primaryKey"`
-	Name			string		`json:"name" gorm:"type:varchar(255);unique;not null"`
-	Province		string		`json:"province" gorm:"type:varchar(255);not null"`
+	Name			string		`json:"name" gorm:"unique;not null"`
+	Province		string		`json:"province" gorm:"not null"`
 	Address			string		`json:"address" gorm:"not null"`
 	Price			int			`json:"price" gorm:"not null"`
-	Google_map		string		`json:"map" gorm:"type:varchar(50);not null"`
+	Google_map		string		`json:"map" gorm:"not null"`
 	Phone_number	string		`json:"phone_number" gorm:"not null"`
 	Web_site		string		`json:"site"`
 	Time			string		`json:"Date" gorm:"not null"`
