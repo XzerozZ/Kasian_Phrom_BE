1da24ab59f6a7ee22bfdde8015e6aafadaa97059
XzerozZ <111548296+XzerozZ@users.noreply.github.com>
2025-03-16 22:00:36 +0700

    Look up

--- modules/nursing_house/usecases/nursing_house_usecase.go
+++ modules/nursing_house/usecases/nursing_house_usecase.go
@@ -311,6 +311,8 @@ func (u *NhUseCaseImpl) RecommendationLLM(userID string) ([]entities.NursingHous
 
 	nhName := nhHistory.NursingHouse.Name
 	urlStr := fmt.Sprintf("%s/llm?nh_name=%s", u.recom, nhName)
+	fmt.Println("u.recom:", u.recom)
+	fmt.Println("nhName:", nhName)
 	resp, err := http.Get(urlStr)
 	if err != nil {
 		return nil, err
